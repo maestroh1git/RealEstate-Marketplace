867cf6ec50d8061bd8fded85ba5c74eebbf99833
David Amaku <amaku@Davids-MacBook-Pro.local>
2022-04-24 18:18:42 +0100

    TestSquareVerifier completed

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 // migrating the appropriate contracts
 const ERC721MintableComplete = artifacts.require('ERC721Mintable');
-//var SquareVerifier = artifacts.require("./SquareVerifier.sol");
+var SquareVerifier = artifacts.require("./SquareVerifier.sol");
 //var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ERC721MintableComplete)
-  // deployer.deploy(SquareVerifier);
+  deployer.deploy(SquareVerifier);
   // const squareVerifier = await SquareVerifier.deployed();
   // deployer.deploy(SolnSquareVerifier, squareVerifier.address);
 };
